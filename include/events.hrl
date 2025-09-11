ce7311e77ce62389462a7bec584c5ef5a5313ba2
Peter Farber <farberpete@gmail.com>
2025-09-11 13:16:28 -0400

    print only if ENV is set

--- include/events.hrl
+++ include/events.hrl
@@ -1,4 +1,18 @@
 
--define(event(X), io:format("[~p:~p:~p] ~p~n", [?MODULE, ?FUNCTION_NAME, ?LINE, X])).
--define(event(Topic, X), io:format("[~p:~p:~p:~p] ~p~n", [Topic, ?MODULE, ?FUNCTION_NAME, ?LINE, X])).
--define(event(Topic, X, Opts), io:format("[~p:~p:~p:~p] ~p ~p~n", [maps:get(topic, Opts, Topic), ?MODULE, ?FUNCTION_NAME, ?LINE, X, Opts])).
+-define(event(X), 
+    case os:getenv("SSL_CERT_DEBUG") of
+        false -> ok;
+        _ -> io:format("[~p:~p:~p] ~p~n", [?MODULE, ?FUNCTION_NAME, ?LINE, X])
+    end).
+
+-define(event(Topic, X), 
+    case os:getenv("SSL_CERT_DEBUG") of
+        false -> ok;
+        _ -> io:format("[~p:~p:~p:~p] ~p~n", [Topic, ?MODULE, ?FUNCTION_NAME, ?LINE, X])
+    end).
+
+-define(event(Topic, X, Opts), 
+    case os:getenv("SSL_CERT_DEBUG") of
+        false -> ok;
+        _ -> io:format("[~p:~p:~p:~p] ~p ~p~n", [maps:get(topic, Opts, Topic), ?MODULE, ?FUNCTION_NAME, ?LINE, X, Opts])
+    end).
